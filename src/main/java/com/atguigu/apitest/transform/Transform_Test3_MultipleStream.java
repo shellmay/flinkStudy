965fdab20109852691f8ba89a4954cc41ef28c1d
shellmay <1147258759@qq.com>
2022-12-18 16:08:05 +0800

    connect and Comap

---
+++ src/main/java/com/atguigu/apitest/transform/Transform_Test3_MultipleStream.java
@@ -0,0 +1,77 @@
+package com.atguigu.apitest.transform;
+
+import com.atguigu.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
+
+import java.util.Collections;
+
+public class Transform_Test3_MultipleStream {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //从文件读取数据路径
+        DataStream<String> inputStream = env.readTextFile("/Users/xiaomeichen/study/2022-study/tools_code/flinkStudy/src/main/resources/sensor.txt");
+
+        //lambda方式
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] sensor = line.split(",");
+
+            return new SensorReading(sensor[0], new Long(sensor[1]), new Double(sensor[2]));
+        });
+        //按照温度分类，高温和低温；
+        SplitStream<SensorReading> splitStream = dataStream.split(new OutputSelector<SensorReading>() {
+            @Override
+            public Iterable<String> select(SensorReading value) {
+                return (value.getTemperature() > 30) ? Collections.singletonList("high") : Collections.singletonList("low");
+            }
+        });
+        DataStream<SensorReading> lowTempStream = splitStream.select("low");
+        DataStream<SensorReading> highTempStream = splitStream.select("high");
+        DataStream<SensorReading> allTempStream = splitStream.select("low", "high");
+
+
+        lowTempStream.print("low");
+        highTempStream.print("high");
+        allTempStream.print("all");
+
+
+        //高温告警（Connect&CoMap）
+        //河流connect
+
+        SingleOutputStreamOperator<Tuple2<String, Double>> warningStream = highTempStream.map(new MapFunction<SensorReading, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(SensorReading value) throws Exception {
+                return new Tuple2<>(value.getId(), value.getTemperature());
+            }
+        });
+
+        ConnectedStreams<Tuple2<String, Double>, SensorReading> connectedStreams = warningStream.connect(lowTempStream);
+
+        SingleOutputStreamOperator<Object> resultStream = connectedStreams.map(new CoMapFunction<Tuple2<String, Double>, SensorReading, Object>() {
+            @Override
+            public Object map1(Tuple2<String, Double> value) throws Exception {
+                return new Tuple3<>(value.f0, value.f1, "warning");
+            }
+
+            @Override
+            public Object map2(SensorReading value) throws Exception {
+                return new Tuple2<>(value.getId(), "healthy");
+            }
+        });
+        resultStream.print();
+
+
+        env.execute();
+
+
+    }
+}
